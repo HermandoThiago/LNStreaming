1b9f7e619c62480fcfaccf4ea7b96ce580fbb70f
HermandoThiago <hermandodev@gmail.com>
2025-07-12 16:16:05 -0300

    feat: site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,204 @@
-import Image from "next/image";
+import { ShoppingCart } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const streamings = [
+    {
+      name: "globoplay + canais",
+      preco: 15.0,
+      cor: "red-400",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMIf5OeYfz9FDHG9gpy3tyXdDpSjVBG_wrlA&s",
+    },
+    {
+      name: "youtube premium",
+      preco: 10.0,
+      cor: "red-400",
+      imagem:
+        "https://www.pngitem.com/pimgs/m/436-4360375_youtube-premium-logo-png-transparent-background-hd-youtube.png",
+    },
+    {
+      name: "prime video",
+      preco: 10.0,
+      cor: "blue-400",
+      imagem:
+        "https://i0.wp.com/cloud.estacaonerd.com/wp-content/uploads/2020/10/12194108/Amazon-Prime.png?fit=512%2C512&ssl=1",
+    },
+    {
+      name: "paramount+",
+      preco: 8.0,
+      cor: "blue-400",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdqGnPo4GJlQN8kYNJdVwxoOWIK-sdZ0Nt4w&s",
+    },
+    {
+      name: "Clarotv básica",
+      preco: 10.0,
+      cor: "red-400",
+      imagem:
+        "https://images.prismic.io/ofertasclaro/ZiFdNPPdc1huKnyR_ClaroTV%2B.jpg?auto=format,compress&rect=225,0,658,658&w=640&h=640",
+    },
+    {
+      name: "Clarotv completa",
+      preco: 30.0,
+      cor: "red-400",
+      imagem:
+        "https://images.prismic.io/ofertasclaro/ZiFdNPPdc1huKnyR_ClaroTV%2B.jpg?auto=format,compress&rect=225,0,658,658&w=640&h=640",
+    },
+    {
+      name: "tv express",
+      preco: 20.0,
+      cor: "purple-purple",
+      imagem:
+        "https://jiroyukabkjzhlkjldur.supabase.co/storage/v1/object/public/products//efe9b243-91e6-40a1-98b5-c6973653f32b.jpg",
+    },
+    {
+      name: "MAX",
+      preco: 10.0,
+      cor: "blue-400",
+      imagem:
+        "https://standfirst-designweek-production.imgix.net/uploads/2023/04/19112920/DixonBaxi_MAX_002-1.jpg?fit=crop&crop=faces&q=80&auto=compress,format&w=364&h=404&dpr=2.625",
+    },
+    {
+      name: "Sky+ básica",
+      preco: 20.0,
+      cor: "red-400",
+      imagem:
+        "https://play-lh.googleusercontent.com/2Tg6j_V3goWRiht2-UEGUXuOeJ2uLTIgPqUhLrDsrkqL5Y2Ac_zCNEZUHELNoqYqww",
+    },
+    {
+      name: "Sky+ completa",
+      preco: 40.0,
+      cor: "red-400",
+      imagem:
+        "https://play-lh.googleusercontent.com/2Tg6j_V3goWRiht2-UEGUXuOeJ2uLTIgPqUhLrDsrkqL5Y2Ac_zCNEZUHELNoqYqww",
+    },
+    {
+      name: "My Family Cinema",
+      preco: 10.0,
+      cor: "orange-400",
+      imagem:
+        "https://http2.mlstatic.com/D_NQ_NP_722141-MLB78705571733_082024-O.webp",
+    },
+    {
+      name: "Disney+",
+      preco: 15.0,
+      cor: "blue-400",
+      imagem:
+        "https://i.pinimg.com/736x/2b/fc/f1/2bfcf1f53b5d6fc10998509152011368.jpg",
+    },
+    {
+      name: "Unitv",
+      preco: 20.0,
+      cor: "white",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-mKu57_q3iYR8LlR03Ta3ofCHghBTO8vTQ&s",
+    },
+    {
+      name: "Telecine",
+      preco: 8.0,
+      cor: "blue-400",
+      imagem:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT74vf8_giSk0vCB4SnqzlRGVyq4TDWUvZQhvF4zsQFAHHSf4JsCncmPxc&s=10",
+    },
+    {
+      name: "Premiere",
+      preco: 10.0,
+      cor: "green-400",
+      imagem:
+        "https://s3.glbimg.com/v1/AUTH_36abb2af534644878388f516c38b89ac/prod/home-share-1b75cdaa.png",
+    },
+    {
+      name: "Spotify",
+      preco: 12.0,
+      cor: "green-400",
+      imagem:
+        "https://files.tecnoblog.net/wp-content/uploads/2015/07/spotify-novo-verde.jpg",
+    },
+    {
+      name: "Warez TV",
+      preco: 30.0,
+      cor: "green-400",
+      imagem: "https://img.olx.com.br/images/47/479547404561225.jpg",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const colorMap: Record<string, string> = {
+    "orange-400": "border-t-orange-400",
+    "red-400": "border-t-red-400",
+    "blue-400": "border-t-blue-400",
+    "purple-purple": "border-t-purple-400", // Corrija aqui se necessário
+    "green-400": "border-t-green-400",
+    white: "border-t-white",
+  };
+
+  return (
+    <main className="w-full h-full">
+      {/* <header className="w-full py-6"></header> */}
+
+      <section className="w-full max-w-full bg-[url('/hero-bg.jpeg')] bg-fill bg-center h-[780px]"></section>
+
+      <section className="w-full flex items-center justify-between flex-row flex-wrap py-8 px-40 bg-neutral-900 gap-20">
+        <div className="text-center">
+          <h5 className="text-4xl font-extrabold">10K+</h5>
+          <p className="">Clientes Satisfeitos</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="text-center">
+          <h5 className="text-4xl font-extrabold">15+</h5>
+          <p className="">Plataformas Disponíveis</p>
+        </div>
+        <div className="text-center">
+          <h5 className="text-4xl font-extrabold">24/7</h5>
+          <p className="">Suporte Premium</p>
+        </div>
+        <div className="text-center">
+          <h5 className="text-4xl font-extrabold">90%</h5>
+          <p className="">Economia</p>
+        </div>
+      </section>
+
+      <section className="w-full flex items-center justify-center py-10 flex-col">
+        <h4 className="text-3xl font-extrabold mb-2">
+          Contas Premium Disponíveis
+        </h4>
+
+        <p className="block text-md text mb-10">
+          Escolha entre as principais plataformas digitais com acesso completo,
+          sem limitações.
+        </p>
+
+        <div className="w-full flex items-center justify-center flex-row flex-wrap gap-8 px-10">
+          {streamings.map((streaming) => (
+            <div
+              className={`px-8 py-8 bg-neutral-900 rounded-lg border-t-8 min-w-[25%] ${
+                colorMap[streaming.cor]
+              }`}
+              key={streaming.name}
+            >
+              <div className="w-full flex items-center justify-start gap-4 mb-6">
+                <img
+                  src={streaming.imagem}
+                  className="w-[60px] h-[60px] rounded-lg"
+                  alt=""
+                />
+
+                {/* {`border-t-${streaming.cor}`} */}
+
+                <p className="uppercase text-xl font-bold">{streaming.name}</p>
+              </div>
+
+              <div className="w-full flex items-center justify-between mb-8">
+                <p className="text-neutral-400">Conta Completa</p>
+                <p className="font-extrabold">R$ {streaming.preco}/mês</p>
+              </div>
+
+              <div className="w-full flex items-center justify-center gap-2 bg-neutral-800 hover:bg-red-400 transform duration-500 py-4 rounded-lg cursor-pointer">
+                <ShoppingCart />
+                <p className="font-extrabold">Comprar agora</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
