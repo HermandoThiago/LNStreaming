afb4230880a1de027e25a09f0645bae07905a6f1
HermandoThiago <hermandodev@gmail.com>
2025-07-19 23:27:20 -0300

    fix: suspense params

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br" className="scroll-smooth antialiased">
       <body className={inter.className} suppressHydrationWarning>
-        {children}
+        <Suspense>{children}</Suspense>
       </body>
     </html>
   );
